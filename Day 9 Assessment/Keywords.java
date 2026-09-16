bc99112c90b562d7ba0b2816318423b0d0dcb1f4
Nanditha-hk <167460499+Nanditha-hk@users.noreply.github.com>
2026-09-16 23:40:36 +0530

    Keywords.java

---
+++ Day 9 Assessment/Keywords.java
@@ -0,0 +1,18 @@
+package KeywordFrameworkImplementation;
+
+import KeywordDriven.KeywordExecutors;
+
+public class Keywords {
+
+    public static void main(String[] args) {
+
+        KeywordExecutors e = new KeywordExecutors();
+
+        e.executeKeyword("LAUNCH BROWSER");
+        e.executeKeyword("OPEN URL");
+        e.executeKeyword("SEARCH");
+        e.executeKeyword("SEARCH BUTTON");
+        e.executeKeyword("CLOSE BROWSER");
+
+    }
+}
